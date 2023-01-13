c5e344593589b51333edf6e33618ecd7e0fc33dc
JessicaBiancajbro <jessicabiancajbro@gmail.com>
2023-01-13 14:44:00 -0400

    exercicio7

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,11 +7,13 @@ export default class Card extends React.Component {
     const { id, title, thumbnail, price } = this.props;
 
     return (
-      <div data-testid="product" className="product-card">
-        <Link to={ `/product/${id}` }>
+      <div  className="product-card" data-testid="product">
+        <Link 
+        data-testid="product-detail-link" 
+        to={ `/product/${id}` }>
           <img src={ thumbnail } alt={ title } />
         </Link>
-        <Link to={ `/product/${id}` }>
+        <Link  to={ `/product/${id}` }>
           <h3>{ title }</h3>
         </Link>
         <div className="product-price">
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -3,24 +3,38 @@ import Header from '../components/Header';
 import { getProductById } from '../services/api';
 
 export default class Product extends React.Component {
-  state = { isLoading: true };
+  state = { isLoading: true, data:{} };
 
   componentDidMount() {
     const { match: { params: { id } } } = this.props;
-    const data = getProductById(id);
+    this.buscarProduto(id);
+  }
+
+  buscarProduto = async (id) => {
+    const data = await getProductById(id);
     this.setState((currentState) => ({
       ...currentState,
       isLoading: false,
-      ...data,
+      data,
     }));
   }
-
   render() {
+    const { isLoading, data } = this.state;
     return (
       <div>
         <Header { ...this.props } />
         <div className="container">
-          Produto
+          { isLoading ? "Carregando" : (
+            <>
+            <img data-testid="product-detail-image" src={ data.pictures[0].secure_url } />
+             <span data-testid="product-detail-name">{ data.title }</span>
+             <span  data-testid="product-detail-price"> { data.price }</span>
+             <button data-testid="shopping-cart-button">Adicionar ao carrinho</button>
+            </>
+            
+          ) }
+         
+
         </div>
       </div>
     );
